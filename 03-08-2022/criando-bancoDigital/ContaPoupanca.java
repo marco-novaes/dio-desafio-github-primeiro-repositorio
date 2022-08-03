4627ffcf430502faca875ffd45ce7e8c90cf5e8d
Marco Novaes <marco-novaes@uol.com.br>
2022-08-03 11:33:33 -0300

    Create ContaPoupanca.java

---
+++ 03-08-2022/criando-bancoDigital/ContaPoupanca.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Marco Novaes 
+ * @version 02/08/2022
+ */
+public class ContaPoupanca extends Conta
+{
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+    
+    @Override
+   public void imprimirExtrato(){
+        System.out.println("*** Extrato Conta Poupanca ***");
+        super.imprimirInfosComuns();
+    }
+    
+    
+
+}
