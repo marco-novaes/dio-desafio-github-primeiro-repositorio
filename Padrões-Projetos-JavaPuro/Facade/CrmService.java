36a1758b2841777744b167be0e575ca01fc1a75b
Marco Novaes <marco-novaes@uol.com.br>
2022-08-04 11:41:17 -0300

    Create CrmService.java

---
+++ Padrões-Projetos-JavaPuro/Facade/CrmService.java
@@ -0,0 +1,21 @@
+package subsistema1;
+
+
+/**
+ * 
+ * @author Marco Novaes 
+ * @version 04/08/2022
+ */
+public class CrmService
+{
+    private CrmService(){
+        super();
+    }
+    public static void gravarCliente(String nome, String cep, String cidade, String estado){
+        System.out.println("Cliente salvo no sistema de CRM:");
+        System.out.println(nome);
+        System.out.println(cep);
+        System.out.println(cidade);
+        System.out.println(estado);
+    }
+}
