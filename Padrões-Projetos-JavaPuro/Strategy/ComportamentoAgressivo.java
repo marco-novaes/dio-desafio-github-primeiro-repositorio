3c8dba54d7b725cef4836b407592d0a6aa42c067
Marco Novaes <marco-novaes@uol.com.br>
2022-08-04 11:08:39 -0300

    Create ComportamentoAgressivo.java

---
+++ Padrões-Projetos-JavaPuro/Strategy/ComportamentoAgressivo.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @author Marco Novaes 
+ * @version 04/08/2022
+ */
+public class ComportamentoAgressivo implements Comportamento
+{
+    @Override
+    public void mover(){
+        System.out.println("Movendo-se agressivamente...");
+    }
+}
