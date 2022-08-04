9f9228df88c7383013d7414492214b123b9ce3e6
Marco Novaes <marco-novaes@uol.com.br>
2022-08-04 11:08:04 -0300

    Create ComportamentoDefensivo.java

---
+++ Padrões-Projetos-JavaPuro/Strategy/ComportamentoDefensivo.java
@@ -0,0 +1,13 @@
+
+/**
+ * 
+ * @author Marco Novaes 
+ * @version 04/08/2022
+ */
+public class ComportamentoDefensivo implements Comportamento
+{
+     @Override
+    public void mover(){
+        System.out.println("Movendo-se defensivamente...");
+    }
+}
