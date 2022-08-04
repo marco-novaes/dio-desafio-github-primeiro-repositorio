90b760a6c8350f177a75451709ccc6dbcc735dc0
Marco Novaes <marco-novaes@uol.com.br>
2022-08-04 11:07:34 -0300

    Create ComportamentoNormal.java

---
+++ Padrões-Projetos-JavaPuro/Strategy/ComportamentoNormal.java
@@ -0,0 +1,15 @@
+
+/**
+ * 
+ * @author Marco Novaes 
+ * @version 04/08/2022
+ */
+public class ComportamentoNormal implements Comportamento{
+    
+    @Override
+    public void mover(){
+        System.out.println("Movendo-se normalmente...");
+    }
+    
+    
+}
