b06afa5e4f3803378757880028ebe5968208c1dc
Marco Novaes <marco-novaes@uol.com.br>
2022-08-04 08:50:09 -0300

    Create SingletonEager.java

---
+++ 04-08-2022/Design-Pattern-Java-Puro/SingletonEager.java
@@ -0,0 +1,19 @@
+
+/**
+ * Singleton "apressado"
+ * 
+ * @author Marco Novaes 
+ * @version 04/08/2022
+ */
+public class SingletonEager
+{
+    private static SingletonEager instancia = new SingletonEager();
+    
+    private SingletonEager(){
+        super();
+    }
+    
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+}
